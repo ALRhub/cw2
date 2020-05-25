0ff27c992de6d1d6053dab408111df51377545ab
Maximilian Li <maximilian.xiling.li@gmail.com>
2020-05-25 17:22:18 +0200

    foreman now works with job implementations

--- foreman.py
+++ foreman.py
@@ -2,9 +2,11 @@ from workload import config, job
 
 
 class Foreman:
-    def __init__(self, config_path, delete_old_files: bool = False, root_dir: str = ""):
+    def __init__(self, config_path, job_cls: job.AbstractJob, delete_old_files: bool = False, root_dir: str = ""):
         self.config = config.Config(config_path)
         self.jobs = []
 
         for exp_c in self.config.exp_configs:
-            self.jobs.append(job.Job(exp_c, delete_old_files, root_dir))
+            j = job_cls()
+            j._assign(exp_c, delete_old_files, root_dir)
+            self.jobs.append(j)
