122c021f4527837d7a779a6dd39db93d1df1b2d5
Maximilian Li <maximilian.xiling.li@gmail.com>
2020-06-01 19:32:33 +0200

    rename setup py name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='cluster_work_v2',
+    name='cw2',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
