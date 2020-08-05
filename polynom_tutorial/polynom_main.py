4c5568704b71b794746f28e659f181cf791c2c0a
Maximilian Li <maximilian.xiling.li@gmail.com>
2020-08-05 15:14:21 +0200

    extend polynom config with grid

--- polynom_tutorial/polynom_main.py
+++ polynom_tutorial/polynom_main.py
@@ -12,7 +12,7 @@ class Polynomial(experiment.AbstractIterativeExperiment):
         y = config.params.x_0 + config.params.x_1 * x + config.params.x_2 * (x**2)
 
         y_noise = y + (random.randint(-100, 100) / 100.0) * config.params.noise
-
+        
         return {"true_y": y, "sample_y": y_noise}
 
     def save_state(self, config: dict, rep: int, n: int) -> None:
