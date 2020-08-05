ae444ce3d43ce7b942e38f656378cc677fed9464
Maximilian Li <maximilian.xiling.li@gmail.com>
2020-08-05 15:44:17 +0200

    plotting example

--- polynom_tutorial/polynom_load.py
+++ polynom_tutorial/polynom_load.py
@@ -1,6 +1,13 @@
+import matplotlib.pyplot as plt
+
 from cw2 import cluster_work
 
 if __name__ == "__main__":
     cw = cluster_work.ClusterWork(None)
-    d = cw.load()
-    print(d)
+    res = cw.load()
+    
+    for job in res.keys():
+        for logger in res[job].keys():
+            single_df = res[job][logger]
+            single_df[["sample_y", "true_y"]].plot.line()
+            plt.savefig(job + "plot.png")
