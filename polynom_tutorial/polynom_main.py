f6e710ff715c48f27614975236bb4e96dba570b5
Maximilian Li <maximilian.xiling.li@gmail.com>
2020-08-05 14:58:57 +0200

    polynom example wip

---
+++ polynom_tutorial/polynom_main.py
@@ -0,0 +1,27 @@
+from cw2 import cluster_work, experiment
+import random
+
+class Polynomial(experiment.AbstractIterativeExperiment):
+    # ...
+
+    def initialize(self, config: dict, rep: int) -> None:
+        print("Ready to start repetition {}. Resetting everything.".format(rep))
+
+    def iterate(self, config: dict, rep: int, n: int) -> dict:
+        x = config.params.stepsize * n
+        y = config.params.x_0 + config.params.x_1 * x + config.params.x_2 * (x**2)
+
+        y_noise = y + random.randint(-100, 100) * config.params.noise
+
+        return {"true_y": y, "sample_y": y_noise}
+
+    def save_state(self, config: dict, rep: int, n: int) -> None:
+        pass
+
+    def finalize(self):
+        print("Finished. Closing Down.")
+
+
+if __name__ == "__main__":
+    cw = cluster_work.ClusterWork(Polynomial)
+    cw.run()
