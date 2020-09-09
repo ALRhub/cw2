8cc3a7d032f6aa1fde62f91ace41c1dc10a3f102
Maximilian Li <maximilian.xiling.li@gmail.com>
2020-09-09 14:21:42 +0200

    increase version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description='A reengineered framework to run experiments on an computing cluster.',
     long_description=long_description,
@@ -62,7 +62,7 @@ setup(
     keywords=['scientific', 'experiments',
               'distributed computing', 'mpi', 'research'],
 
-    packages=['cw2'],
+    packages=['cw2', 'cw2.cw_data'],
     package_data={'cw2': ['default_sbatch.sh']},
 
     install_requires=['PyYAML', 'numpy', 'pandas', 'attrdict'],
