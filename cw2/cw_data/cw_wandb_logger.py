b50d86aae21c94b2ff0c0defff9853c4e4b5225c
philipp <philippbecker93@googlemail.com>
2021-04-19 21:37:45 +0200

    improved wandb logger

--- cw2/cw_data/cw_wandb_logger.py
+++ cw2/cw_data/cw_wandb_logger.py
@@ -1,8 +1,8 @@
 import os
 import attrdict as ad
-# import pandas as pd
 import wandb
 from cw2.cw_data import cw_logging
+import warnings
 
 
 def reset_wandb_env():
@@ -17,25 +17,28 @@ def reset_wandb_env():
 
 
 class WandBLogger(cw_logging.AbstractLogger):
+
     def __init__(self, ignore_keys: list = []):
+        super(WandBLogger, self).__init__(ignore_keys=ignore_keys)
         self.log_path = ""
-        self.ignore_keys = ignore_keys
         self.run = None
-        #self.index = 0
 
     def initialize(self, config: ad.AttrDict, rep: int, rep_log_path: str) -> None:
-        self.log_path = rep_log_path
-        self.config = ad.AttrDict(config.wandb)
-        reset_wandb_env()
-        job_name = config['_experiment_name'].replace("__", "_")
-        self.run = wandb.init(project=config.wandb.project,
-                              group=config.wandb.group,
-                              job_type=job_name,
-                              name=job_name + "_rep_{:02d}".format(rep),
-                              config=config,
-                              dir=rep_log_path,
-                              settings=wandb.Settings(_disable_stats=config.wandb.disable_stats)
-                              )
+        if "wandb" in config.keys():
+            self.log_path = rep_log_path
+            self.config = ad.AttrDict(config.wandb)
+            reset_wandb_env()
+            job_name = config['_experiment_name'].replace("__", "_")
+            self.run = wandb.init(project=config.wandb.project,
+                                  group=config.wandb.group,
+                                  job_type=job_name,
+                                  name=job_name + "_rep_{:02d}".format(rep),
+                                  config=config.params,
+                                  dir=rep_log_path,
+                                  settings=wandb.Settings(_disable_stats=config.wandb.get("disable_stats", False))
+                                  )
+        else:
+            warnings.warn("No 'wandb' field in yaml - Ignoring Weights & Biases Logger")
 
     def process(self, data: dict) -> None:
         if "histogram" in self.config:
